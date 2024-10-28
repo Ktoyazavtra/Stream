7fac8d8b4097cb73ca83db0e3c86e5f65ef9e418
bogdan <bkormin@gmail.com>
2024-10-28 12:21:25 +0500

    fff

--- Bogdan/App.py
+++ Bogdan/App.py
@@ -6,5 +6,5 @@ st.title('Bogdan')
 VIDEO_URL = "https://www.youtube.com/watch?v=7pxqOmxPgPM"
 st.video(VIDEO_URL)
 st.write('Ritter')
-img = Image.open('Alexey-Savostin.jpg')
+img = Image.open('./Bogdan/Alexey-Savostin.jpg')
 st.image(img)
